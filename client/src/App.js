071c910e5fb18ac9019839b08e330f1a6849959d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-12 17:34:38 +0200

    Step2:functionality-line13 changed rest incomplete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import {Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,9 +16,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path="/" component={'MovieList'}/>
-      <Route path="/movies" component={'Movie'}/> 
+      <Route exact path="/" component={'MovieList'}/>
       <Route path="/movies/:id" component={'Movie'}/>
+    
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
+
+
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -18,6 +20,7 @@ const MovieList = props => {
     
     getMovies();
   }, []);
+ 
   
   return (
     <div className="movie-list">
@@ -29,8 +32,8 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
+  const { title, director, metascore, stars, id } = movie;
+  return (  
     <Link to={`/movies/${id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
@@ -48,7 +51,10 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
-    </Link>
+    </Link> 
+
+    
+    
   );
 }
 
